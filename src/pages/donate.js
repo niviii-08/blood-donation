2ed7e5e871111ad7577db166f7150eda7ba01818
niviii-08 <neevetha2006@gmail.com>
2025-09-13 22:32:19 +0530

    Initial commit

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,16 +1,21 @@
-// pages/donate.js
-import Head from 'next/head';
+// src/pages/donate.js
+import Head from "next/head";
+import Script from "next/script";
+import "@/styles/donate.css"; // ✅ move donate.css into src/styles/
 
 export default function Donate() {
     return (
         <>
             <Head>
-                <link rel="stylesheet" href="/donate.css" />
-                <script src="/donate.js" defer></script>
+                <title>Donate Blood</title>
             </Head>
+
             <main>
                 {/* Paste your HTML body content here */}
             </main>
+
+            {/* ✅ Load your custom JS properly */}
+            <Script src="/js/donate.js" strategy="afterInteractive" />
         </>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,20 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Script from "next/script";
+import "@/styles/bloodconnect.css"; // move CSS into src/styles/ instead of public/css
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>BloodConnect</title>
-        <link rel="stylesheet" href="/css/bloodconnect.css" />
       </Head>
 
       <div>
         {/* Paste your HTML body content here */}
       </div>
 
-      <script src="/js/bloodconnect.js"></script>
+      {/* Load your JS correctly */}
+      <Script src="/js/bloodconnect.js" strategy="afterInteractive" />
     </>
-  )
+  );
 }
