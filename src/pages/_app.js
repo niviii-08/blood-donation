69598b0b73d84504fde24fbb311facc4148ebf6d
niviii-08 <neevetha2006@gmail.com>
2025-09-13 22:41:27 +0530

    final commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
-import "@/styles/globals.css";
+import "@/styles/globals.css";        // already exists usually
+import "@/styles/bloodconnect.css";  // ✅ add your global styles here
+import "@/styles/donate.css";
+import "@/styles/find-donors.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
